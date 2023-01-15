cc8fc8fd67993f3bea711602e38f12533cf7991a
VdeThevenin <renato.bittencourt13@gmail.com>
2023-01-15 18:07:57 -0300

    Add Serial

---
+++ Serial.py
@@ -0,0 +1,121 @@
+import time
+
+import serial
+import numpy as np
+from serial.tools import list_ports
+
+VIDPIDs = set([(0x0483, 0x5740), (0x04b4, 0x0008)])
+
+class Serial:
+    def __init__(self, dev=None):
+        self.dev = dev or getport()
+        self.serial = None
+        self._frequencies = None
+        self.points = 101
+
+    @property
+    def frequencies(self):
+        return self._frequencies
+
+    def set_frequencies(self, start=1e6, stop=900e6, points=None):
+        if points:
+            self.points = points
+        self._frequencies = np.linspace(start, stop, self.points)
+
+    def open(self):
+        if self.serial is None:
+            self.serial = serial.Serial(self.dev)
+
+    def close(self):
+        if self.serial:
+            self.serial.close()
+        self.serial = None
+
+    def send_command(self, cmd):
+        self.open()
+        self.serial.write(cmd.encode())
+        self.serial.readline()  # discard empty line
+
+    def set_sweep(self, start, stop):
+        if start is not None:
+            self.send_command("sweep start %d\r" % start)
+        if stop is not None:
+            self.send_command("sweep stop %d\r" % stop)
+
+    def set_frequency(self, freq):
+        if freq is not None:
+            self.send_command("freq %d\r" % freq)
+
+    def set_port(self, port):
+        if port is not None:
+            self.send_command("port %d\r" % port)
+
+    def set_gain(self, gain):
+        if gain is not None:
+            self.send_command("gain %d %d\r" % (gain, gain))
+
+    def set_offset(self, offset):
+        if offset is not None:
+            self.send_command("offset %d\r" % offset)
+
+    def set_strength(self, strength):
+        if strength is not None:
+            self.send_command("power %d\r" % strength)
+
+    def set_filter(self, filter):
+        self.filter = filter
+
+    def fetch_data(self):
+        result = ''
+        line = ''
+        while True:
+            c = self.serial.read().decode('utf-8')
+            if c == chr(13):
+                continue  # ignore CR
+            line += c
+            if c == chr(10):
+                result += line
+                line = ''
+                continue
+            if line.endswith('ch>'):
+                # stop on prompt
+                break
+        return result
+
+    def fetch_buffer(self, freq=None, buffer=0):
+        self.send_command("dump %d\r" % buffer)
+        data = self.fetch_data()
+        x = []
+        for line in data.split('\n'):
+            if line:
+                x.extend([int(d, 16) for d in line.strip().split(' ')])
+        return np.array(x, dtype=np.int16)
+
+    def fetch_rawwave(self, freq=None):
+        if freq:
+            self.set_frequency(freq)
+            time.sleep(0.05)
+        self.send_command("dump 0\r")
+        data = self.fetch_data()
+        x = []
+        for line in data.split('\n'):
+            if line:
+                x.extend([int(d, 16) for d in line.strip().split(' ')])
+        return np.array(x[0::2], dtype=np.int16), np.array(x[1::2], dtype=np.int16)
+
+    def fetch_array(self, sel):
+        self.send_command("data %d\r" % sel)
+        data = self.fetch_data()
+        x = []
+        for line in data.split('\n'):
+            if line:
+                x.extend([float(d) for d in line.strip().split(' ')])
+        return np.array(x[0::2]) + np.array(x[1::2]) * 1j
+
+
+def getport() -> str:
+    device_list = list_ports.comports()
+    for device in device_list:
+        if (device.vid, device.pid) in VIDPIDs:
+            return device.device
+    raise OSError("device not found")
